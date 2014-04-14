94daae62f409fa4a910f856ed8c4d1f8dd827958
James Hunter <james.hunter@me.com>
2014-04-14 18:02:34 +0100

    More Heroku prep

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module QuizProject
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = true
-    config.assets.initialize_on_precompile = false
+    config.cache_classes = true
+    config.serve_static_assets = true
+    config.assets.compile = true
+    config.assets.digest = true
   end
 end
